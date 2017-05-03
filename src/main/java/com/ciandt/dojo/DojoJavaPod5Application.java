d75af09957742194b281815438f112524aeba935
root <root@CIT016894.cit>
2017-05-03 12:38:15 +0000

    Criando o repositorio

---
+++ src/main/java/com/ciandt/dojo/DojoJavaPod5Application.java
@@ -0,0 +1,12 @@
+package com.ciandt.dojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoJavaPod5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoJavaPod5Application.class, args);
+	}
+}
